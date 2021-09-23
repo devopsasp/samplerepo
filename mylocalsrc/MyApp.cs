918adb816938303c6f3511d02efc7eefae78f481
devopsasp <85219013+devopsasp@users.noreply.github.com>
2021-09-23 17:06:58 +0530

    Update MyApp.cs

--- mylocalsrc/MyApp.cs
+++ mylocalsrc/MyApp.cs
@@ -4,7 +4,7 @@ public class MyApp
     public static void main(String args[]) 
   {
 
-    Console.WriteLine("Hello World!Welcome to my programming in C# again programming!Happy coding!"); 
+    Console.WriteLine("Hello World!Welcome to console programming !Happy coding!"); 
 
 
 } 
