677b6b15fae070605468b8426b3fca45be9bdd8c
devopsasp <85219013+devopsasp@users.noreply.github.com>
2021-09-23 17:35:35 +0530

    Update MyApp.cs

--- mylocalsrc/MyApp.cs
+++ mylocalsrc/MyApp.cs
@@ -4,7 +4,7 @@ public class MyApp
     public static void main(String args[]) 
   {
 
-    Console.WriteLine("Hello World!merged second line for coding !Happy coding!"); 
+    Console.WriteLine("Hello World!This is a sample app!Happy coding!"); 
 
 
 } 
