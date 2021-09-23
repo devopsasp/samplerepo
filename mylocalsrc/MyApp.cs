3291e891f78f93a96592cdc1787970bf7f5aa212
A.Sailesh Padmanabhan <arjun_sailesh@yahoo.com>
2021-09-23 15:40:54 +0530

    Update MyApp.cs

--- mylocalsrc/MyApp.cs
+++ mylocalsrc/MyApp.cs
@@ -7,6 +7,10 @@ public class MyApp
     Console.WriteLine("Hello World!Happy coding"); 
 =======
     Console.WriteLine("Hello World!Welcome to programming"); 
+<<<<<<< HEAD
 >>>>>>> 272ad916a501cf041dd624ee72ba3dd6ca68b1db
+=======
+>>>>>>> 272ad91 (Update MyApp.cs)
+>>>>>>> f3ccbaf (Update MyApp.cs)
 } 
  } 
